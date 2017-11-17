eac14a38a918398e67d9b937e5d4196cc001331b
Jared M <mitten.13@osu.edu>
2017-11-16 21:57:54 -0500

    Add project files.

---
+++ XmasFadecandy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XmasFadecandy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
